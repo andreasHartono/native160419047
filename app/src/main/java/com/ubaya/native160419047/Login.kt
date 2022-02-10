474a5203229b47c6757b77017e5498f6d1e912e6
andreas <s160419047@student.ubaya.ac.id>
2022-02-10 15:07:16 +0700

    toString override updated

--- app/src/main/java/com/ubaya/native160419047/Login.kt
+++ app/src/main/java/com/ubaya/native160419047/Login.kt
@@ -2,6 +2,6 @@ package com.ubaya.native160419047
 
 class Login(var username: String,var password: String) {
     override fun toString(): String {
-        return "Hai Namaku $username aku temannya Orang"
+        return "Hai Namaku $username aku berasal dari $password"
     }
 }
